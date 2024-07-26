6f9aac4baca2e72328e703f2abe7fc8502fbea67
Proscka <proskinaviktoria861@gmail.com>
2024-07-26 10:19:36 +0300

    Add files via upload

---
+++ Namespace/main.py
@@ -0,0 +1,16 @@
+def test_function():
+
+    def inner_funсtion():
+        print("Я в области видимости функции test_function")
+
+
+print("Я в области видимости функции test_function")
+test_function()
+
+
+
+
+
+
+
+
